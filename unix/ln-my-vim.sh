8de7283fb7ce85293091c0bfa7859b3249c37938
binesiyu <yubinexsaber@gmail.com>
2017-05-12 14:44:41 +0800

    + ln-my-vim

---
+++ unix/ln-my-vim.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+ln  vim-self/dist/ctags_lang ~/.ctags
+ln  vim-self/.vimrc ~/.vimrc
+ln  vim-self/.vimrc.plugins ~/.vimrc.plugins
+ln  vim-self/.vimrc.local ~/.vimrc.local
+ln  vim-self/.vimrc.ex ~/.vimrc.ex
+ln  vim-self/.vimrc.haskell ~/.vimrc.haskell
+ln  vim-self/.vimrc.plugins.local ~/.vimrc.plugins.local
+# rm -rf ~/.vim
+# cp -r vimfiles ~/.vim
