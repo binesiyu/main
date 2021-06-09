f3a83f3bb21f794759d04f4da6b7724ca75a2a3c
yubin <1960028060@qq.com>
2021-06-09 19:24:52 +0800

    telescope

--- nvim.lua
+++ nvim.lua
@@ -72,15 +72,25 @@ require("packer").startup(function(use)
 
     -- Telescope
     use {"nvim-lua/popup.nvim", opt = true}
+    require_plugin("popup.nvim")
+
     use {"nvim-lua/plenary.nvim", opt = true}
+    require_plugin("plenary.nvim")
+
     use {"nvim-telescope/telescope.nvim", opt = true}
     use {"nvim-telescope/telescope-fzy-native.nvim", opt = true}
-    use {"nvim-telescope/telescope-project.nvim", opt = true}
-
-    require_plugin("popup.nvim")
-    require_plugin("plenary.nvim")
     require_plugin("telescope.nvim")
+
+    use {"nvim-telescope/telescope-project.nvim", opt = true}
     require_plugin('telescope-project.nvim')
+
+    if isLinux() then
+        use {"nvim-telescope/telescope-media-files.nvim", opt = true}
+        require_plugin('telescope-media-files.nvim')
+    end
+
+    use {"folke/trouble.nvim", opt = true}
+    require_plugin('trouble.nvim')
 
     -- Treesitter
     useAndRequire(use,{"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"},"nvim-treesitter")
@@ -89,6 +99,8 @@ require("packer").startup(function(use)
 
     -- ui
     useAndRequire(use,{"mhinz/vim-startify", opt = true},"vim-startify")
+    -- Icons
+    useAndRequire(use,{"kyazdani42/nvim-web-devicons", opt = true},"nvim-web-devicons")
 
     -- colorscheme
     use {"morhetz/gruvbox", opt = true}
@@ -308,6 +320,18 @@ map('<','<gv','v',noremap_cfg)
 --Use tab for indenting in visual mode
 map('<Tab>','>gv|','x',noremap_cfg)
 map('<S-Tab>','<gv','x',noremap_cfg)
+
+-- " Improve scroll, credits: https://github.com/Shougo
+vim.cmd([[
+nnoremap <expr> zz (winline() == (winheight(0)+1) / 2) ?
+            \ 'zt' : (winline() == &scrolloff + 1) ? 'zb' : 'zz'
+noremap <expr> <C-f> max([winheight(0) - 2, 1])
+            \ ."\<C-d>".(line('w$') >= line('$') ? "L" : "H")
+noremap <expr> <C-b> max([winheight(0) - 2, 1])
+            \ ."\<C-u>".(line('w0') <= 1 ? "H" : "L")
+noremap <expr> <C-e> (line("w$") >= line('$') ? "j" : "4\<C-e>")
+noremap <expr> <C-y> (line("w0") <= 1         ? "k" : "4\<C-y>")
+]])
 
 map('<Space>','<C-e>','n')
 map('<S-Space>','<C-y>','n')
@@ -529,3 +553,101 @@ require'nvim-treesitter.configs'.setup {
       },
   },
 }
+
+
+--telescope
+local actions = require('telescope.actions')
+local trouble = require("trouble.providers.telescope")
+-- Global remapping
+------------------------------
+-- '--color=never',
+require('telescope').setup {
+    defaults = {
+        find_command = {'rg', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case'},
+        prompt_position = "bottom",
+        -- prompt_prefix = " ",
+        prompt_prefix = " ",
+        selection_caret = " ",
+        entry_prefix = "  ",
+        initial_mode = "insert",
+        -- initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "descending",
+        layout_strategy = "horizontal",
+        layout_defaults = {horizontal = {mirror = false}, vertical = {mirror = false}},
+        file_sorter = require'telescope.sorters'.get_fzy_sorter,
+        file_ignore_patterns = {},
+        generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
+        shorten_path = true,
+        winblend = 0,
+        width = 0.75,
+        preview_cutoff = 120,
+        results_height = 1,
+        results_width = 0.8,
+        border = {},
+        borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
+        color_devicons = true,
+        use_less = true,
+        set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil,
+        file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+        grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+        qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+
+        -- Developer configurations: Not meant for general override
+        buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
+        mappings = {
+            i = {
+                ["<C-c>"] = actions.close,
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<c-t>"] = trouble.open_with_trouble,
+                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+                -- To disable a keymap, put [map] = false
+                -- So, to not map "<C-n>", just put
+                -- ["<c-x>"] = false,
+                -- ["<esc>"] = actions.close,
+
+                -- Otherwise, just set the mapping to the function that you want it to be.
+                -- ["<C-i>"] = actions.select_horizontal,
+
+                -- Add up multiple actions
+                ["<CR>"] = actions.select_default + actions.center
+
+                -- You can perform as many actions in a row as you like
+                -- ["<CR>"] = actions.select_default + actions.center + my_cool_custom_action,
+            },
+            n = {
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<c-t>"] = trouble.open_with_trouble,
+                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist
+                -- ["<C-i>"] = my_cool_custom_action,
+            }
+        }
+    },
+    extensions = {
+        fzy_native = {
+            override_generic_sorter = false,
+            override_file_sorter = true
+        },
+        media_files = {
+              -- filetypes whitelist
+              -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+              filetypes = {"png", "webp", "jpg", "jpeg"},
+              find_cmd = "rg" -- find command (defaults to `fd`)
+        },
+    }
+
+}
+
+require'telescope'.load_extension('project')
+if isLinux() then
+    require'telescope'.load_extension('media_files')
+end
+
+-- Find files using Telescope command-line sugar.
+map('<C-P>',':Telescope find_files<cr>','n',noremap_cfg)
+map('<leader>ff',':Telescope find_files<cr>','n',noremap_cfg)
+map('<leader>fg',':Telescope live_grep<cr>','n',noremap_cfg)
+map('<leader>fb',':Telescope buffers<cr>','n',noremap_cfg)
+map('<leader>fh',':Telescope help_tags<cr>','n',noremap_cfg)
